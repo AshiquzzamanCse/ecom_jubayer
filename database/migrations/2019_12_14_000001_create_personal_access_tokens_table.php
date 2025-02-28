081871116875bf0816ce3464dd5cac5f4c2b7eb5
ashiquzzaman77 <ashiquzzaman.rajib.cse@gmail.com>
2025-02-28 22:11:57 +0600

    update by ashiquzzaman home 27-02-2025

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -9,11 +9,14 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+     /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable'); // Creates tokenable_type and tokenable_id
+            $table->morphs('tokenable');
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
@@ -21,16 +24,13 @@ return new class extends Migration
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
-
-        // Modify the index length for tokenable_type to 191 characters
-        Schema::table('personal_access_tokens', function (Blueprint $table) {
-            $table->index(['tokenable_type', 'tokenable_id'], 'personal_access_tokens_tokenable_type_tokenable_id_index', ['length' => ['tokenable_type' => 191]]);
-        });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('personal_access_tokens');
     }
-
 };
