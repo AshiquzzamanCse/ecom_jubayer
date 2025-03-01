72fa22cc620893f5c7e0aa8fb2ddb516aa9c42bf
ashiquzzaman77 <ashiquzzaman.rajib.cse@gmail.com>
2025-03-01 12:21:04 +0600

    update by ashiquzzaman home 27-02-2025

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -16,15 +16,21 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable',191);
+            // Manually define the 'tokenable_type' and 'tokenable_id' columns
+            $table->string('tokenable_type', 191); // Set the length to 191 to avoid index size issues
+            $table->unsignedBigInteger('tokenable_id');
             $table->string('name');
             $table->string('token', 255)->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
+    
+            // Add index for tokenable_type and tokenable_id (after manually defining the columns)
+            $table->index(['tokenable_type', 'tokenable_id']); // Create an index for the morphs fields
         });
     }
+    
 
     /**
      * Reverse the migrations.
